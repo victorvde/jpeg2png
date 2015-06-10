b1e5e6bb0ace7cc326970e86be631b522ef91c38
Victor van den Elzen <victor.vde@gmail.com>
2015-06-11 00:30:24 +0200

    refactor compute a bit

--- compute.c
+++ compute.c
@@ -16,7 +16,7 @@
   #define compute_step_tv compute_step_tv_c
 #endif
 
-POSSIBLY_UNUSED static double compute_step_tv_c(unsigned w, unsigned h, float *in, float *objective_gradient, float *in_x, float *in_y) {
+POSSIBLY_UNUSED static double compute_step_tv_c(unsigned w, unsigned h, float *in, float *obj_gradient, float *in_x, float *in_y) {
         double tv = 0.;
         for(unsigned y = 0; y < h; y++) {
                 for(unsigned x = 0; x < w; x++) {
@@ -29,12 +29,12 @@ POSSIBLY_UNUSED static double compute_step_tv_c(unsigned w, unsigned h, float *i
                         tv += g_norm;
                         // compute derivatives
                         if(g_norm != 0) {
-                                *p(objective_gradient, x, y, w, h) += -(g_x + g_y) / g_norm;
+                                *p(obj_gradient, x, y, w, h) += -(g_x + g_y) / g_norm;
                                 if(x < w-1) {
-                                        *p(objective_gradient, x+1, y, w, h) += g_x / g_norm;
+                                        *p(obj_gradient, x+1, y, w, h) += g_x / g_norm;
                                 }
                                 if(y < h-1) {
-                                        *p(objective_gradient, x, y+1, w, h) += g_y / g_norm;
+                                        *p(obj_gradient, x, y+1, w, h) += g_y / g_norm;
                                 }
                         }
                         *p(in_x, x, y, w, h) = g_x;
@@ -44,22 +44,22 @@ POSSIBLY_UNUSED static double compute_step_tv_c(unsigned w, unsigned h, float *i
         return tv;
 }
 
-POSSIBLY_UNUSED static void verify_compute_step_tv(unsigned w, unsigned h, double tv, float *in, float *objective_gradient, float *in_x, float *in_y) {
+POSSIBLY_UNUSED static void verify_compute_step_tv(unsigned w, unsigned h, double tv, float *in, float *obj_gradient, float *in_x, float *in_y) {
         puts("verify");
-        float *objective_gradient_ = alloc_real(w*h);
+        float *obj_gradient_ = alloc_real(w*h);
         float *in_x_ = alloc_real(w*h);
         float *in_y_ = alloc_real(w*h);
-        double tv_c = compute_step_tv_c(w, h, in, objective_gradient_, in_x_, in_y_);
+        double tv_c = compute_step_tv_c(w, h, in, obj_gradient_, in_x_, in_y_);
         compare("in_x", w, h, in_x_, in_x);
         compare("in_y", w, h, in_y_, in_y);
-        compare("objective_gradient", w, h, objective_gradient_, objective_gradient);
+        compare("obj_gradient", w, h, obj_gradient_, obj_gradient);
         printf("simd %f, original %f\n", tv, tv_c);
-        free_real(objective_gradient_);
+        free_real(obj_gradient_);
         free_real(in_y_);
         free_real(in_x_);
 }
 
-static double compute_step_tv2(unsigned w, unsigned h, float *objective_gradient, float *in_x, float *in_y, float alpha) {
+static double compute_step_tv2(unsigned w, unsigned h, float *obj_gradient, float *in_x, float *in_y, float alpha) {
         double tv2 = 0.;
         for(unsigned y = 0; y < h; y++) {
                 for(unsigned x = 0; x < w; x++) {
@@ -76,24 +76,24 @@ static double compute_step_tv2(unsigned w, unsigned h, float *objective_gradient
                         tv2 += g2_norm;
                         // compute derivatives
                         if(g2_norm != 0.) {
-                                *p(objective_gradient, x, y, w, h) += alpha * (-(2. * g_xx + g_xy + g_yx + 2. *  g_yy) / g2_norm);
+                                *p(obj_gradient, x, y, w, h) += alpha * (-(2. * g_xx + g_xy + g_yx + 2. *  g_yy) / g2_norm);
                                 if(x > 0) {
-                                        *p(objective_gradient, x-1, y, w, h) += alpha * ((g_yx + g_xx) / g2_norm);
+                                        *p(obj_gradient, x-1, y, w, h) += alpha * ((g_yx + g_xx) / g2_norm);
                                 }
                                 if(x < w-1) {
-                                        *p(objective_gradient, x+1, y, w, h) += alpha * ((g_xx + g_xy) / g2_norm);
+                                        *p(obj_gradient, x+1, y, w, h) += alpha * ((g_xx + g_xy) / g2_norm);
                                 }
                                 if(y > 0) {
-                                        *p(objective_gradient, x, y-1, w, h) += alpha * ((g_yy + g_xy) / g2_norm);
+                                        *p(obj_gradient, x, y-1, w, h) += alpha * ((g_yy + g_xy) / g2_norm);
                                 }
                                 if(y < h-1) {
-                                        *p(objective_gradient, x, y+1, w, h) += alpha * ((g_yy + g_yx) / g2_norm);
+                                        *p(obj_gradient, x, y+1, w, h) += alpha * ((g_yy + g_yx) / g2_norm);
                                 }
                                 if(x < w-1 && y > 0) {
-                                        *p(objective_gradient, x+1, y-1, w, h) += alpha * ((-g_xy) / g2_norm);
+                                        *p(obj_gradient, x+1, y-1, w, h) += alpha * ((-g_xy) / g2_norm);
                                 }
                                 if(x > 0 && y < h-1) {
-                                        *p(objective_gradient, x-1, y+1, w, h) += alpha * ((-g_yx) / g2_norm);
+                                        *p(obj_gradient, x-1, y+1, w, h) += alpha * ((-g_yx) / g2_norm);
                                 }
                         }
                 }
@@ -101,27 +101,31 @@ static double compute_step_tv2(unsigned w, unsigned h, float *objective_gradient
         return tv2;
 }
 
-static double compute_step(unsigned w, unsigned h, float *in, float *out, float step_size, float weight, float *objective_gradient, float *in_x, float *in_y, struct logger *log) {
+static double compute_step(unsigned w, unsigned h, float *in, float *out, float step_size, float weight, float *temp[3], struct logger *log) {
         float alpha = weight / sqrt(4. / 2.);
+        float *obj_gradient = temp[0];
+        float *in_x = temp[1];
+        float *in_y = temp[2];
+
         for(unsigned i = 0; i < h * w; i++) {
-                objective_gradient[i] = 0.;
+                obj_gradient[i] = 0.;
         }
 
-        double tv = compute_step_tv(w, h, in, objective_gradient, in_x, in_y);
+        double tv = compute_step_tv(w, h, in, obj_gradient, in_x, in_y);
 #ifdef SIMD_VERIFY
-        verify_compute_step_tv(w, h, tv, in, objective_gradient, in_x, in_y);
+        verify_compute_step_tv(w, h, tv, in, obj_gradient, in_x, in_y);
 #endif
 
-        double tv2 = alpha == 0. ? 0. : compute_step_tv2(w, h, objective_gradient, in_x, in_y, alpha);
+        double tv2 = alpha == 0. ? 0. : compute_step_tv2(w, h, obj_gradient, in_x, in_y, alpha);
 
         float norm = 0.;
         for(unsigned i = 0; i < h * w; i++) {
-                norm += sqr(objective_gradient[i]);
+                norm += sqr(obj_gradient[i]);
         }
         norm = sqrt(norm);
 
         for(unsigned i = 0; i < h * w; i++) {
-                out[i] = in[i] - step_size * (objective_gradient[i] /  norm);
+                out[i] = in[i] - step_size * (obj_gradient[i] /  norm);
         }
 
         double objective = (tv + alpha * tv2) / (alpha + 1.);
@@ -130,61 +134,78 @@ static double compute_step(unsigned w, unsigned h, float *in, float *out, float 
         return objective;
 }
 
-struct compute_projection_aux {
+struct compute_aux {
         float *q_min;
         float *q_max;
-        float *temp;
+        float *cos;
+        float *temp[3];
+        float *fista;
 };
 
-static void compute_projection_init(unsigned w, unsigned h, int16_t *data, uint16_t quant_table[64], struct compute_projection_aux *aux) {
+static void compute_aux_init(unsigned w, unsigned h, int16_t *data, uint16_t quant_table[64], float *fdata, struct compute_aux *aux) {
         float *q_max = alloc_real(h * w);
         if(!q_max) { die("allocation error"); }
         float *q_min = alloc_real(h * w);
         if(!q_min) { die("allocation error"); }
         unsigned blocks = (h / 8) * (w / 8);
-
         for(unsigned i = 0; i < blocks; i++) {
                 for(unsigned j = 0; j < 64; j++) {
-                       q_max[i*64+j] = (data[i*64+j] + 0.5) * quant_table[j];
-                       q_min[i*64+j] = (data[i*64+j] - 0.5) * quant_table[j];
+                        q_max[i*64+j] = (data[i*64+j] + 0.5) * quant_table[j];
+                        q_min[i*64+j] = (data[i*64+j] - 0.5) * quant_table[j];
                 }
         }
-
         aux->q_min = q_min;
         aux->q_max = q_max;
 
-        float *temp = alloc_real(h * w);
-        if(!temp) { die("allocation error"); }
+        float *cos = alloc_real(h * w);
+        if(!cos) { die("allocation error"); }
+        for(unsigned i = 0; i < blocks; i++) {
+                for(unsigned j = 0; j < 64; j++) {
+                        cos[i*64+j] = data[i*64+j] * quant_table[j];
+                }
+        }
+        aux->cos = cos;
 
-        aux->temp = temp;
+        for(unsigned i = 0; i < 3; i++) {
+                float *t = alloc_real(h * w);
+                if(!t) { die("allocation error"); }
+                aux->temp[i] = t;
+        }
+
+        float *fista = alloc_real(h * w);
+        if(!fista) { die("allocation error"); }
+        memcpy(fista, fdata, sizeof(float) * w * h);
+        aux->fista = fista;
 }
 
-static void compute_projection_destroy(struct compute_projection_aux *aux) {
-        free_real(aux->temp);
+static void compute_aux_destroy(struct compute_aux *aux) {
+        free_real(aux->cos);
         free_real(aux->q_min);
         free_real(aux->q_max);
+        for(unsigned i = 0; i < 3; i++) {
+                free_real(aux->temp[i]);
+        }
+        free_real(aux->fista);
 }
 
-static void compute_projection(unsigned w, unsigned h, float *fdata, struct compute_projection_aux *aux) {
-        float *temp = aux->temp;
-
+static void compute_projection(unsigned w, unsigned h, float *fdata, float *cos, float *q_min, float *q_max) {
         unsigned blocks = (h / 8) * (w / 8);
 
-        box(fdata, temp, w, h);
+        box(fdata, cos, w, h);
 
         for(unsigned i = 0; i < blocks; i++) {
-                dct8x8s(&temp[i*64]);
+                dct8x8s(&cos[i*64]);
         }
 
         for(unsigned i = 0; i < h * w; i++) {
-                temp[i] = CLAMP(temp[i], aux->q_min[i], aux->q_max[i]);
+                cos[i] = CLAMP(cos[i], q_min[i], q_max[i]);
         }
 
         for(unsigned i = 0; i < blocks; i++) {
-                idct8x8s(&temp[i*64]);
+                idct8x8s(&cos[i*64]);
         }
 
-        unbox(temp, fdata, w, h);
+        unbox(cos, fdata, w, h);
 }
 
 void compute(struct coef *coef, struct logger *log, struct progressbar *pb, uint16_t quant_table[64], float weight, unsigned iterations) {
@@ -193,19 +214,8 @@ void compute(struct coef *coef, struct logger *log, struct progressbar *pb, uint
         float *fdata = coef->fdata;
         ASSUME_ALIGNED(fdata);
 
-        struct compute_projection_aux cpa;
-        compute_projection_init(w, h, coef->data, quant_table, &cpa);
-
-        float *temp_x = alloc_real(h * w);
-        if(!temp_x) { die("allocation error"); }
-        float *temp_y = alloc_real(h * w);
-        if(!temp_y) { die("allocation error"); }
-        float *temp_gradient = alloc_real(h * w);
-        if(!temp_gradient) { die("allocation error"); }
-
-        float *temp_fista = alloc_real(h * w);
-        if(!temp_fista) { die("allocation error"); }
-        memcpy(temp_fista, fdata, sizeof(float) * w * h);
+        struct compute_aux aux;
+        compute_aux_init(w, h, coef->data, quant_table, fdata, &aux);
 
         float radius = sqrt(w*h) / 2;
         for(unsigned i = 0; i < iterations; i++) {
@@ -213,15 +223,15 @@ void compute(struct coef *coef, struct logger *log, struct progressbar *pb, uint
 
                 float k = i;
                 for(unsigned j = 0; j < w * h; j++) {
-                        temp_fista[j] = fdata[j] + (k - 2.)/(k+1.) * (fdata[j] - temp_fista[j]);
+                        aux.fista[j] = fdata[j] + (k - 2.)/(k+1.) * (fdata[j] - aux.fista[j]);
                 }
 
-                compute_step(w, h, temp_fista, temp_fista, radius / sqrt(1 + iterations), weight, temp_x, temp_y, temp_gradient, log);
-                compute_projection(w, h, temp_fista, &cpa);
-
                 float *t = fdata;
-                fdata = temp_fista;
-                temp_fista = t;
+                fdata = aux.fista;
+                aux.fista = t;
+
+                compute_step(w, h, fdata, fdata, radius / sqrt(1 + iterations), weight, aux.temp, log);
+                compute_projection(w, h, fdata, aux.cos, aux.q_min, aux.q_max);
 
                 if(pb) {
 #ifdef USE_OPENMP
@@ -232,10 +242,5 @@ void compute(struct coef *coef, struct logger *log, struct progressbar *pb, uint
         }
 
         coef->fdata = fdata;
-        free_real(temp_x);
-        free_real(temp_y);
-        free_real(temp_gradient);
-        free_real(temp_fista);
-
-        compute_projection_destroy(&cpa);
+        compute_aux_destroy(&aux);
 }
